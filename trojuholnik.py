208c5101c1c8c5940ec9aed284da002b4313bd73
samuelxb30 <samuel.barat77@gmail.com>
2026-09-06 21:36:13 +0200

    trojuholnik

---
+++ trojuholnik.py
@@ -0,0 +1,23 @@
+while True:
+ akysposobinput = int(input("Ked chces trojuholnik od najvacieho po najmensi stlac 1 a ked opacne tak 2 ---> "))
+                #Ten int len meni hodnotu zo stringu na cislo :D
+
+ def prvysposob():
+   znak = "X"
+
+   for i in range(20, 0, -1):
+    print(znak*i)
+   print("") #kvôli oddeleniu
+
+ def druhysposob():
+  znak = "X"
+
+  for i in range(0, 20, 1):
+    print(znak*i)
+  print("") #kvôli oddeleniu
+
+ if akysposobinput == 1:
+  prvysposob()
+
+ if akysposobinput == 2:
+  druhysposob()
