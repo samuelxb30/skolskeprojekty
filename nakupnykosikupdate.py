e247c3533633b9909b9d3c71c04f0542c2189b07
samuelxb30 <samuel.barat77@gmail.com>
2026-09-23 21:27:27 +0200

    WIP done

---
+++ nakupnykosikupdate.py
@@ -0,0 +1,62 @@
+ovocie = ["marhula", "jablko"]
+zelenina = ["petrzlen", "mrkva"]
+sladkosti = ["cukor", "cokolada"]
+
+
+
+polozkyaceny = {
+  "jablko": 1,
+  "marhula": 1.5,
+  "banan": 1.3,
+  "petrzlen": 2,
+  "cukor": 2.3,
+  "mrkva": 2,
+  "cokolada": 3,
+  "rohlik": 0.10
+}
+
+kosik = []
+
+print("Dobry den, nech sa paci mozte si vybrat\n")
+for polozka, cena in polozkyaceny.items():
+  print(polozka, cena)
+
+while True:
+      
+   vyber = input("co chces pridat? ---> ")
+
+   if vyber in polozkyaceny:
+    cena = polozkyaceny[vyber]
+    kosik.append([vyber, cena])
+
+   elif vyber.lower() == "uz nic" or vyber.lower() == "nothing":
+     anoniezobrazenieinput = input("Chces zobrazit kosik? ---> ")
+
+     if anoniezobrazenieinput.lower() == "ano":
+         spolu = 0
+
+         for polozka, cena in kosik:
+           spolu += cena
+       
+           print("\n--------------------")
+           for polozka, cena in kosik:
+            if polozka in ovocie:
+             kategoria = "ovocie"
+               
+            elif polozka in zelenina:
+             kategoria = "zelenina"
+               
+            elif polozka in sladkosti:
+             kategoria = "sladkost"
+               
+            else:
+             kategoria = "ine"
+
+            print(f"{polozka} - {cena} € - {kategoria}")
+           print("      -------")
+           print(f"spolu: {spolu} €")
+     elif anoniezobrazenieinput.lower() == "nie":
+       break
+
+   else:
+    print("Taku vec nemame")
